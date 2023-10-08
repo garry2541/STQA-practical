cecbb7b9cec473ea623f77edf0e83befd100a331
Giridhar Balakrishnan <147298473+ggdadda@users.noreply.github.com>
2023-10-08 20:48:45 +0530

    java file

--- prac5/Excelreader.java
+++ prac5/Excelreader.java
@@ -1,4 +1,5 @@
-package excelread;
+package faizan5;
+
 import java.io.File;
 import java.io.IOException;
 import jxl.Cell;
@@ -6,49 +7,44 @@ import jxl.CellType;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
-public class Excelreader {
-private String inputFile;
-public void setInputFile(String inputFile) {this.inputFile = inputFile;}
-public void read() throws IOException {
-File inputWorkbook = new File(inputFile);
-Workbook w;
-boolean flag=false;
-int count=0;
-try {
-w = Workbook.getWorkbook(inputWorkbook);
-// Get the first sheet
-Sheet sheet = w.getSheet(0);
-// Loop over first 10 column and lines
-for (int j = 0; j < sheet.getRows(); j++) {
-for (int i = 0; i < sheet.getColumns()-1; i++) {
-Cell cell = sheet.getCell(i, j);
-if (cell.getType() == CellType.NUMBER) {
 
-if(Integer.parseInt(cell.getContents())>=60){
-flag = true;
-if(flag == true){
-count++;
-flag=false;
-}
-break;
-}
-}
-}
-}
-System.out.println("Total number of students who scored more
+public class faizan5 {
+	private String inputFile;
+	public void setInputFile(String inputFile) {this.inputFile = inputFile;}
+	public void read() throws IOException {
+	File inputWorkbook = new File(inputFile);
+	Workbook w;
+	boolean flag=false;
+	int count=0;
+	try {
+	w = Workbook.getWorkbook(inputWorkbook);
+	// Get the first sheet
+	Sheet sheet = w.getSheet(0);
+	// Loop over first 10 column and lines
+	for (int j = 0; j < sheet.getRows(); j++) {
+	for (int i = 0; i < sheet.getColumns()-1; i++) {
+	Cell cell = sheet.getCell(i, j);
+	if (cell.getType() == CellType.NUMBER) {
 
-than 60 in one or more subjects: " +count);
+	if(Integer.parseInt(cell.getContents())>=60){
+	flag = true;
+	if(flag == true){
+	count++;
+	flag=false;
+	}
+	break;
+	}	}	}	}
+	System.out.println("Total number of students who scored more than 60 in one or more subjects: " +count);
 
-}
-catch (BiffException e) {e.printStackTrace();}
-}
+	}
+	catch (BiffException e) {e.printStackTrace();}
+	}
 
-public static void main(String[] args) throws IOException {
-Excelreader test = new Excelreader();
+	public static void main(String[] args) throws IOException {
+		faizan5 test = new faizan5();
 
-test.setInputFile("C:\\Users\\Usman\\eclipse-
-workspace\\p5\\student.xls");
+	test.setInputFile("Q:\\OneDrive\\Desktop\\Preliums Practical\\STQA\\Pract_4\\student.xls");
 
-test.read();
-}
+	test.read();
+	}
 }
