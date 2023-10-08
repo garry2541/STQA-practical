376030814ff318b481273554a6bf2dae2822bbac
Agent 355 <147298473+ggdadda@users.noreply.github.com>
2023-10-08 22:00:50 +0530

    java file

---
+++ prac8/MultiChecker.java
@@ -0,0 +1,45 @@
+package prac8;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.*;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+
+public class prac8 {
+	static String driverPath = "D:\\My things\\TYCS\\STQA\\geckodriver.exe";
+	public static void main(String[] args) {
+	System.setProperty("webdriver.gecko.driver",driverPath);
+	//DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+	//capabilities.setCapability("marionette",true);
+	//ProfilesIni allProfiles = new ProfilesIni();
+	//FirefoxProfile fp = new FirefoxProfile();
+	//fp.setPreference(FirefoxProfile.PORT_PREFERENCE,"7055");
+	//FirefoxOptions options = new FirefoxOptions();
+	//options.setProfile(fp);
+	WebDriver driver = new FirefoxDriver();
+
+	//String appUrl = "https://www.toolsqa.com/automation-practice-form/";
+
+	//DYNAMIC URL(WEBSITE)
+
+	String appUrl ="file:D:\\My things\\TYCS\\STQA\\index.html"; //STATIC URL(LOCALFILE)
+
+	driver.get(appUrl);
+
+	List<WebElement> checkBoxes =
+	driver.findElements(By.xpath("//input[@type='checkbox']"));
+	int checkedCount=0, uncheckedCount=0;
+	for(int i=0; i<checkBoxes.size(); i++){
+	System.out.println(i+" checkbox is selected"+checkBoxes.get(i).isSelected());
+	if(checkBoxes.get(i).isSelected()) {checkedCount++;}
+	else {uncheckedCount++;}
+	}
+	//driver.quit();
+	System.out.println("No. of selected checkbox: "+checkedCount);
+	System.out.println("No. of unselected checkbox: "+uncheckedCount);
+	}
+}
