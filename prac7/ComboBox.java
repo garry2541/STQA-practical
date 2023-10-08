f7719ea1334adaf9cea42897d374a1891675d120
Giridhar Balakrishnan <147298473+ggdadda@users.noreply.github.com>
2023-10-08 21:04:00 +0530

    java file

--- prac7/ComboBox.java
+++ prac7/ComboBox.java
@@ -1,4 +1,5 @@
-package p9;
+package pract_7;
+
 import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,47 +10,48 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
 import org.openqa.selenium.support.ui.Select;
-public class ComboBox {
-static String driverPath = "C:\\Users\\Usman\\503\\geckodriver.exe";
-public static void main(String[] args) {
-System.setProperty("webdriver.gecko.driver",driverPath);
-//NOTE THAT: following commented lines are required for old machines
-//DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-//capabilities.setCapability("marionette",true);
-//ProfilesIni allProfiles = new ProfilesIni();
-//FirefoxProfile fp = new FirefoxProfile();
-//fp.setPreference(FirefoxProfile.PORT_PREFERENCE,"7055");
-//FirefoxOptions options = new FirefoxOptions();
 
-//options.setProfile(fp);
+public class pract7 {
+	
+	static String driverPath = "D:\\My things\\TYCS\\STQA\\geckodriver.exe";
+	public static void main(String[] args) {
+	System.setProperty("webdriver.gecko.driver",driverPath);
+	//NOTE THAT: following commented lines are required for old machines
+	//DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+	//capabilities.setCapability("marionette",true);
+	//ProfilesIni allProfiles = new ProfilesIni();
+	//FirefoxProfile fp = new FirefoxProfile();
+	//fp.setPreference(FirefoxProfile.PORT_PREFERENCE,"7055");
+	//FirefoxOptions options = new FirefoxOptions();
 
-WebDriver driver = new FirefoxDriver();
-String appUrl ="https://www.toolsqa.com/automation-practice-form/";
+	//options.setProfile(fp);
 
-//DYNAMIC URL(WEBSITE)
+	WebDriver driver = new FirefoxDriver();
+	//String appUrl ="https://www.toolsqa.com/automation-practice-form/";
 
-//String appUrl = "
+	//DYNAMIC URL(WEBSITE)
 
-file:///D:/Usman/College/503%20pracs/combobox.html"; //STATIC URL(LOCAL FILE)
+	String appUrl = "file:C:\\Users\\Admin\\eclipse-workspace\\pract_7\\src\\pract_7\\index.html"; //STATIC URL(LOCAL FILE)
 
-driver.get(appUrl);
-Select oSelect = new
+	driver.get(appUrl);
+	Select oSelect = new
 
-Select(driver.findElement(By.id("continents")));
+	Select(driver.findElement(By.id("continents")));
 
-//Select oSelect = new
+	//Select oSelect = new
 
-Select(driver.findElement(By.tagName("select"))); //this works too
-List<WebElement> oSize = oSelect.getOptions();
-int iListSize = oSize.size();
-for(int i =0; i < iListSize ; i++)
-{
-// Storing the value of the option
-String sValue = oSelect.getOptions().get(i).getText();
-// Printing the stored value
-System.out.println(sValue);
-}
-System.out.println("Total No. Items in Dropdown: "+iListSize);
-//driver.quit();
-}
-}
+	
+	List<WebElement> oSize = oSelect.getOptions();
+	int iListSize = oSize.size();
+	for(int i =0; i < iListSize ; i++)
+	{
+	// Storing the value of the option
+	String sValue = oSelect.getOptions().get(i).getText();
+	// Printing the stored value
+	System.out.println(sValue);
+	}
+	System.out.println("Total No. Items in Dropdown: "+iListSize);
+	//driver.quit();
+	}
+	}
+
