6696e76ed36f703a504bf2d6acf31137d9899885
Agent 355 <147298473+ggdadda@users.noreply.github.com>
2023-10-09 06:38:00 +0530

    update MultiChecker.java

--- prac8/MultiChecker.java
+++ prac8/MultiChecker.java
@@ -1,4 +1,4 @@
-package prac8;
+package practical8;
 import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,37 +8,20 @@ import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.firefox.internal.ProfilesIni;
-
 public class prac8 {
 	static String driverPath = "D:\\My things\\TYCS\\STQA\\geckodriver.exe";
 	public static void main(String[] args) {
 	System.setProperty("webdriver.gecko.driver",driverPath);
-	//DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-	//capabilities.setCapability("marionette",true);
-	//ProfilesIni allProfiles = new ProfilesIni();
-	//FirefoxProfile fp = new FirefoxProfile();
-	//fp.setPreference(FirefoxProfile.PORT_PREFERENCE,"7055");
-	//FirefoxOptions options = new FirefoxOptions();
-	//options.setProfile(fp);
 	WebDriver driver = new FirefoxDriver();
-
-	//String appUrl = "https://www.toolsqa.com/automation-practice-form/";
-
-	//DYNAMIC URL(WEBSITE)
-
-	String appUrl ="file:D:\\My things\\TYCS\\STQA\\index.html"; //STATIC URL(LOCALFILE)
-
+	String appUrl ="file:D:\\My things\\TYCS\\garrydhar\\index2.html"; 
 	driver.get(appUrl);
-
-	List<WebElement> checkBoxes =
-	driver.findElements(By.xpath("//input[@type='checkbox']"));
+	List<WebElement> checkBoxes =driver.findElements(By.xpath("//input[@type='checkbox']"));
 	int checkedCount=0, uncheckedCount=0;
 	for(int i=0; i<checkBoxes.size(); i++){
 	System.out.println(i+" checkbox is selected"+checkBoxes.get(i).isSelected());
 	if(checkBoxes.get(i).isSelected()) {checkedCount++;}
 	else {uncheckedCount++;}
 	}
-	//driver.quit();
 	System.out.println("No. of selected checkbox: "+checkedCount);
 	System.out.println("No. of unselected checkbox: "+uncheckedCount);
 	}
